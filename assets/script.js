eff0af298e4f22a4e4d79926e2a41dc5cbf35158
James Pritchard <jamespritchard@Jamess-Air.tb.shawcable.net>
2022-05-24 21:22:40 -0400

    time displayed on each timeblock

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,24 @@ var currentDayEL = document.getElementById('currentDay')
 
 const now = dayjs()
 currentDayEL.textContent = now.format("MMM DD YYYY")
-var nineAM = dayjs().hour(9).minute(0).format("H:mm")
-console.log(now.format("H:mm"));
-console.log(nineAM);
-
+timeBlockTimes =[
+    dayjs().hour(9).minute(0).format("H:mm"),
+    dayjs().hour(10).minute(0).format("H:mm"),
+    dayjs().hour(11).minute(0).format("H:mm"),
+    dayjs().hour(12).minute(0).format("H:mm"),
+    dayjs().hour(13).minute(0).format("H:mm"),
+    dayjs().hour(14).minute(0).format("H:mm"),
+    dayjs().hour(15).minute(0).format("H:mm"),
+    dayjs().hour(16).minute(0).format("H:mm")
+]
+ta
 function createAllTimeBlocks() {
 for (var i = 0; i < 8; i++) {
     var timeBlock = $("<div>");
     timeBlock.addClass("time-block mb-1");
-    timeBlock.text(nineAM)
+    timeBlock.attr('id',`timeblock${i}`)
     $('#timeBlockContainer').append(timeBlock);
+    document.querySelector(`#timeblock${i}`).textContent = `${timeBlockTimes[i]}`
 }
 }
 function init(){
