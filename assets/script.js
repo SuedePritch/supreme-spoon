b6f420cd0658b38f8b0bcb6efbef62650a6d2b0f
James Pritchard <jamespritchard@Jamess-Air.tb.shawcable.net>
2022-05-26 13:39:14 -0400

    setup loading events from local bug:not displaying title/desc

--- assets/script.js
+++ assets/script.js
@@ -10,15 +10,15 @@ var closeModalButtonEl = document.getElementById('close-event-container')
 const now = dayjs()
 currentDayEL.textContent = now.format("dddd MMM DD YYYY")
 timeBlockTimes =[
-    dayjs().hour(0).format("H"),
-    dayjs().hour(1).format("H"),
-    dayjs().hour(2).format("H"),
-    dayjs().hour(3).format("H"),
-    dayjs().hour(4).format("H"),
-    dayjs().hour(5).format("H"),
-    dayjs().hour(6).format("H"),
-    dayjs().hour(7).format("H"),
-    dayjs().hour(8).format("H"),
+    // dayjs().hour(0).format("H"),
+    // dayjs().hour(1).format("H"),
+    // dayjs().hour(2).format("H"),
+    // dayjs().hour(3).format("H"),
+    // dayjs().hour(4).format("H"),
+    // dayjs().hour(5).format("H"),
+    // dayjs().hour(6).format("H"),
+    // dayjs().hour(7).format("H"),
+    // dayjs().hour(8).format("H"),
 
     dayjs().hour(9).format("H"),
     dayjs().hour(10).format("H"),
@@ -29,13 +29,13 @@ timeBlockTimes =[
     dayjs().hour(15).format("H"),
     dayjs().hour(16).format("H"),
     
-    dayjs().hour(17).format("H"),
-    dayjs().hour(18).format("H"),
-    dayjs().hour(19).format("H"),
-    dayjs().hour(20).format("H"),
-    dayjs().hour(21).format("H"),
-    dayjs().hour(22).format("H"),
-    dayjs().hour(23).format("H")
+    // dayjs().hour(17).format("H"),
+    // dayjs().hour(18).format("H"),
+    // dayjs().hour(19).format("H"),
+    // dayjs().hour(20).format("H"),
+    // dayjs().hour(21).format("H"),
+    // dayjs().hour(22).format("H"),
+    // dayjs().hour(23).format("H")
 ]
 //Create all time blocks
 //each timeBlock contains two sections
@@ -44,9 +44,11 @@ timeBlockTimes =[
 //Set time for each section
 function createAllTimeBlocks() {
     for (var i = 0; i < timeBlockTimes.length; i++) {
-        var timeBlock = $("<ul>");
-        var hourBlock = $("<li>");
-        var eventBlock = $("<li>");
+        var timeBlock = $("<div>");
+        var hourBlock = $("<div>");
+        var eventBlock = $("<div>");
+        var eventBlockTitle = $("<h2>");
+        var eventBlockDescription = $("<p>");
 
         timeBlock.addClass("time-block");
         timeBlock.attr('id',`timeblock${i}`)
@@ -58,12 +60,23 @@ function createAllTimeBlocks() {
         eventBlock.addClass('event-block')
         eventBlock.attr('id', `event-block${i}`)
 
-        $('#timeBlockContainer').append(timeBlock);
-        timeBlock.append(hourBlock)
-        timeBlock.append(eventBlock)
-        document.querySelector(`#hour-block${i}`).textContent = `${timeBlockTimes[i]}:00`
-        document.querySelector(`#event-block${i}`).textContent = ''
+        eventBlockTitle.addClass('event-block-title')
+        eventBlockTitle.attr('id', `event-block-title${i}`)
+        eventBlockTitle.text('')
+        eventBlockDescription.addClass('event-block-description')
+        eventBlockDescription.attr('id', `event-block-description${i}`)
+        eventBlockDescription.text('')
 
+        
+
+        $('#timeBlockContainer').append(timeBlock);
+        timeBlock.append(hourBlock, eventBlock)
+        timeBlock.append(eventBlockTitle, eventBlockDescription); 
+        
+        
+
+        document.querySelector(`#hour-block${i}`).textContent = `${timeBlockTimes[i]}`
+        document.querySelector(`#event-block${i}`).textContent = ''
         updatePastPresentFuture(i);
         
     }
@@ -98,8 +111,22 @@ function createNewEvent(newEventTitleText, newEventDescriptionText){
     localStorage.setItem(`${eventBlockId}`, JSON.stringify({title:`${newEventTitleText}`, description:`${newEventDescriptionText}`}))
     var currentEventBlock = document.querySelector(`#${eventBlockId}`)
     currentEventBlock.textContent = `${newEventTitleText}`
+}
 
+function getEventsFromLocalAndUpdate(){
+    for(i=0; i < 24; i++){
+        let eventFromLocal = JSON.parse(localStorage.getItem(`event-block${i}`))
+        console.log(eventFromLocal);
+        // let eventFromLocalTitleText = eventFromLocal.title
+        // let eventFromLocalDescriptionText = eventFromLocal.description
 
+        // var tempEventTitleEl = document.getElementById(`event-block${i}`)
+        // tempEventTitleEl.children('#event-block-title').textContent = eventFromLocalTitleText
+        // document.getElementById(`event-block${i}`).textContent = eventFromLocalDescriptionText
+        
+
+        
+    }
 }
 
 
@@ -109,6 +136,7 @@ saveEventButtonEl.addEventListener('click', function(){
     createNewEvent(newEventTitleText, newEventDescriptionText)
     modalEl.setAttribute('style', "display:none")
 })
+
 
 
 
@@ -128,6 +156,7 @@ window.addEventListener('click', function(event){
 
 function init(){
     createAllTimeBlocks();
+    getEventsFromLocalAndUpdate();
 }
 
 init();
